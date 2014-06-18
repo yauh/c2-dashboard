fa6c934aa26358fe9af2edd094be81b7422fdd0c
Stephan Hochhaus <stephan@yauh.de>
2014-06-18 18:49:10 +0200

    added routing and styling
    added bootstrap styling and routing

---
+++ client/router/router.js
@@ -0,0 +1,22 @@
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.map( function () {
+    this.route('home', {
+        path: '/',
+    });
+    this.route('about', {
+        path: '/about'
+    });
+    this.route('upload', {
+        path: '/upload',
+        template: 'uploadCsv',
+    });
+    this.route('dashboard', {
+        path: '/dashboard'
+    });
+    this.route('logcard', {
+        path: '/logcard',
+    });
+});
